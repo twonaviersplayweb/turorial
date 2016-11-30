ac1f22805615553f54c45fb5610608db4378ab80
yooner <yooner@yoonerdeMacBook-Pro.local>
2016-12-01 07:45:19 +0800

    Initial commit

---
+++ turorial/spiders/dmoz_spider.py
@@ -0,0 +1,29 @@
+__author__ = 'yooner'
+
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from turorial.items import DmozItem
+
+
+
+class DmozSpider(Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/",
+    ]
+
+    def parse(self, response):
+        sel = Selector(response)
+        sites = sel.xpath('//ul[@class="directory-url"]/li')
+        items = []
+
+        for site in sites:
+            item = DmozItem()
+            item['name'] = site.xpath('a/text()').extract()
+            item['url'] = site.xpath('a/@href').extract()
+            item['description'] = site.xpath('text()').re('-\s[^\n]*\\r')
+            print(item)
+            items.append(item)
+        return items
