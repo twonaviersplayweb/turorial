5993df1d28f35bfb169e865a445de557cf7a702e
yooner <411890573@qq.com>
2016-12-06 20:49:04 +0800

    建立book数据映射

--- turorial/pipelines.py
+++ turorial/pipelines.py
@@ -4,7 +4,32 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from sqlalchemy import creat_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+
+sqlite_url = ''
+engine = creat_engine(sqlite_url, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
 class TurorialPipeline(object):
     def process_item(self, item, spider):
+        
         return item
+
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    pic_url = Column(String)
+    pub = Column(String)
+    breif = Column(String)
+
+    def __repr(self):
+        return "<Book(name='%s', pic_url='%s', pub='%s', breif='%s')>" % (
+            self.name, self.pic_url, self.pub, self.breif
+        )
